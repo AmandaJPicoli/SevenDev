6dd5c7a3694b48765ef59a5fb24a656842f49c6a
AmandaJPicoli <amandajardimpicoli@gmail.com>
2021-02-16 12:55:31 -0300

    Tabelas sql

--- sql/tabelas.sql
+++ sql/tabelas.sql
@@ -1,2 +1,88 @@
 ﻿
-select * from Genero
+
+--Criação das tabelas
+CREATE TABLE dbo.Genero (
+   Id int IDENTITY(1,1) NOT NULL,
+   Descricao varchar(50) NOT NULL,
+   CONSTRAINT PK_Genero_Id PRIMARY KEY CLUSTERED (Id)
+)
+
+CREATE TABLE dbo.Usuario (
+	Id int IDENTITY(1,1) NOT NULL,
+	GeneroId int NOT NULL,
+	Nome varchar(250) NOT NULL,
+	Email varchar(100) NOT NULL,
+	Senha varchar(200) NOT NULL,
+	DataNascimento DateTime NOT NULL,
+	Foto varchar(max) NOT NULL
+	CONSTRAINT PK_Usuario_Id PRIMARY KEY CLUSTERED (Id)
+)
+
+ALTER TABLE dbo.Usuario
+   ADD CONSTRAINT FK_Usuario_Genero FOREIGN KEY (GeneroId)
+      REFERENCES dbo.Genero (Id)
+
+
+CREATE TABLE dbo.Postagem (
+   Id int IDENTITY(1,1) NOT NULL,
+   UsuarioId int NOT NULL,
+   Texto varchar(250) NOT NULL,
+   Foto varchar(500) NULL,
+   Criacao DateTime NOT NULL,
+   CONSTRAINT PK_Postagem_Id PRIMARY KEY CLUSTERED (Id)
+)
+
+ALTER TABLE dbo.Postagem
+   ADD CONSTRAINT FK_Postagem_Usuario FOREIGN KEY (UsuarioId)
+      REFERENCES dbo.Usuario (Id)
+
+
+CREATE TABLE dbo.Comentario (
+   Id int IDENTITY(1,1) NOT NULL,
+   UsuarioId int NOT NULL,
+   PostagemId int NOT NULL,
+   Texto varchar(250) NOT NULL,
+   Criacao DateTime NOT NULL,
+   CONSTRAINT PK_Comentario_Id PRIMARY KEY CLUSTERED (Id)
+)
+
+ALTER TABLE dbo.Comentario
+   ADD CONSTRAINT FK_Comentario_Usuario FOREIGN KEY (UsuarioId)
+      REFERENCES dbo.Usuario (Id)
+
+ALTER TABLE dbo.Comentario
+   ADD CONSTRAINT FK_Comentario_Postagem FOREIGN KEY (PostagemId)
+      REFERENCES dbo.Postagem (Id)
+
+CREATE TABLE dbo.Curtidas (
+   Id int IDENTITY(1,1) NOT NULL,
+   UsuarioId int NOT NULL,
+   PostagemId int NOT NULL,
+   CONSTRAINT PK_Curtidas_Id PRIMARY KEY CLUSTERED (Id)
+)
+
+ALTER TABLE dbo.Curtidas
+   ADD CONSTRAINT FK_Curtidas_Usuario FOREIGN KEY (UsuarioId)
+      REFERENCES dbo.Usuario (Id)
+
+ALTER TABLE dbo.Curtidas
+   ADD CONSTRAINT FK_Curtidas_Postagem FOREIGN KEY (PostagemId)
+      REFERENCES dbo.Postagem (Id)
+	  
+	  CREATE TABLE Convite(
+  Id INT identity(1,1) NOT NULL,
+  UsuarioIdConvidou int NOT NULL,
+  UsuarioIdRecebeu int NOT NULL, 
+  AceitouConvite bit not null,
+  RecusouConvite bit not null,
+  DataCriacao DateTime not null
+  Constraint Pk_Convite_Id PRIMARY KEY CLUSTERED(Id)
+)
+
+  ALTER TABLE Convite
+   ADD CONSTRAINT FK_Usuario_Convidou FOREIGN KEY (UsuarioIdConvidou)
+      REFERENCES Usuario (Id)
+ALTER TABLE Convite
+   ADD CONSTRAINT FK_Usuario_recebeu FOREIGN KEY (UsuarioIdRecebeu)
+      REFERENCES Usuario (Id);
+
